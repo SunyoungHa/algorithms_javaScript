5d4ac4f0585fd0391ea07978b725e10ac224429a
SunyoungHa <sy@anniecannons.com>
2020-11-05 09:49:15 -0800

    update isBigger.js

--- isBigger/isBigger.js
+++ isBigger/isBigger.js
@@ -2,6 +2,24 @@
 Given two numbers, a, b, as inputs, return true if a > b,
 return false if a < b, return 0 otherwise.
 
+
+declare a function
+	take two inputs as parameter
+	if/else conditionals
+	if a is bigger than b , return false
+	else return 0
+
+
+output
+: true, false
+
+
+call the fuction
+
+
+
+
+
 Bonus 1: If a or b is undefined or not a number, then return -1.
 Hint: Chekout typeof() operator at
 https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/typeof
@@ -40,6 +58,17 @@ console.log(result) // -1
 
 
 function isBigger(a, b) {
-	//Your code here
+	if (a>b){
+		return false
+	}
+	else{
+		return 0
+	}
 	
 }
+
+var result = isBigger(3,4);
+console.log(result) // false
+
+var result = isBigger(13,4);
+console.log(result) // true
--- pseudocode-practice/pseudocode/countByTwos.js
+++ pseudocode-practice/pseudocode/countByTwos.js
@@ -2,3 +2,16 @@
 
 // Version 2: Your product manager wants to increase user engagement by considering user input. Your product manager wants this program to be based on the user's range of minimum and maximum numbers. Display the numbers to the user in the browser.
 // Example: Given a minimum of 5 and a maximum of 10, the browser will display: 6, 8, 10
+
+
+
+function name it. 
+loop to loop throuhg 0-100
+for loopprnt the resulting number to the user in the brower . document.wirte() or 
+
+
+)
+
+
+input: ranege of number from 0 thrugh 100. 
+output: all the even numbers printed 
