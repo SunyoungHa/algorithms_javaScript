cb200006ae6767102b70ba1ad14e09b370b563ec
SunyoungHa <sy@anniecannons.com>
2020-11-10 10:50:15 -0800

    update missing semicolon

--- longestWord.js
+++ longestWord.js
@@ -36,12 +36,12 @@ function findLongestWord(str) {
 
 
 var result1 = findLongestWord("The quick brown fox jumped over the lazy dog");
-var result2 = findLongestWord("The quick brown fox jumped over the lazy dog")
-var result3 = findLongestWord("May the force be with you") 
-var result4 = findLongestWord("The quick brown fox jumped over the lazy dog") 
-var result5 = findLongestWord("Google do a barrel roll")
-var result6 = findLongestWord("What is the average airspeed velocity of an unladen swallow") 
-var result7 = findLongestWord("What if we try a super-long word such as otorhinolaryngology") 
+var result2 = findLongestWord("The quick brown fox jumped over the lazy dog");
+var result3 = findLongestWord("May the force be with you");
+var result4 = findLongestWord("The quick brown fox jumped over the lazy dog"); 
+var result5 = findLongestWord("Google do a barrel roll");
+var result6 = findLongestWord("What is the average airspeed velocity of an unladen swallow") ;
+var result7 = findLongestWord("What if we try a super-long word such as otorhinolaryngology") ;
 
 console.log(result1);
 console.log(result2);
